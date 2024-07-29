9e2f7447d3ef6ac5fe227abff91418ea78196dbd
SeolDoin <tjfehdls@gmail.com>
2024-07-29 16:39:41 +0900

    24.07.29_theme_오류수정_load

--- ROOT/main/js/index.js
+++ ROOT/main/js/index.js
@@ -1,60 +1,63 @@
-const button = document.querySelector('.theme')
-const userTheme = localStorage.getItem('theme')
-let status = false
+const button = document.querySelector('.theme');
+const userTheme = localStorage.getItem('theme');
+let status = false;
 
-// alert(userTheme);
-
-$(document).ready(function(){
-    //실행내용
-
-})
 
 // 처음 이용객의 테마를 읽음
 document.addEventListener('DOMContentLoaded', () => {
-    if (userTheme === 'dark') {
-        clickDarkMode()
-    } else if (userTheme === 'light') {
-        clickLightMode()
-    }
+  // alert(userTheme);
+  clickDarkMode();
+  clickLightMode()
+  if (userTheme === 'dark') {
+      clickDarkMode()
+  } else if (userTheme === 'light') {
+      clickLightMode()
+  }
 })
+
 
 // 버튼클릭
 button.addEventListener('click', () => {
-    if (status === false) {
-        clickDarkMode()
-    } else if (status === true) {
-        clickLightMode()
-    }
+  if (status === false) {
+      clickDarkMode()
+  } else if (status === true) {
+      clickLightMode()
+  }
 })
 
 // 다크/라이트 전환이벤트
 function clickDarkMode() {
-    localStorage.setItem("theme", "dark")
-    document.documentElement.setAttribute('data-theme', 'dark')
-    button.innerText='Light'
-    button.classList.remove('light_bt');
-    button.classList.add('dark_bt');
-    status = true
+  localStorage.setItem("theme", "dark")
+  document.documentElement.setAttribute('data-theme', 'dark')
+  button.innerText='Light'
+  button.classList.remove('light_bt');
+  button.classList.add('dark_bt');
+  status = true
 }
 function clickLightMode() {
-    localStorage.setItem("theme", "light")
-    document.documentElement.setAttribute('data-theme', 'light')
-    button.innerText='Dark'
-    button.classList.remove('dark_bt');
-    button.classList.add('light_bt');
-    status = false
+  localStorage.setItem("theme", "light")
+  document.documentElement.setAttribute('data-theme', 'light')
+  button.innerText='Dark'
+  button.classList.remove('dark_bt');
+  button.classList.add('light_bt');
+  status = false
 }
 
 
 
 
 	/* mobile nav */	
-	$('.side_nav_menu_bt a').on('click', function(e) {
-		e.preventDefault();
-        alert("0");
+	$('.side_nav_menu_bt a').on('click', function() {
+		// e.preventDefault();
+    if($("main").hasClass("side_off")) {
+      $('main').removeClass('side_off');
+      $('main').addClass('side_on');
+    } else {
+      $('main').removeClass('side_on');
+      $('main').addClass('side_off')
+    }
 
-        $('main').removeClass('side_off');
-        $('main').addClass('side_on');
+
 		// var $target = $(this).parent();
 		// $('#mobile_gnb li').not($target).removeClass('on');
 		// $target.toggleClass('on');
