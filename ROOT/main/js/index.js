f2ec33b469f12a28005d5b0d7ebfb160ea7df5b8
SeolDoin <tjfehdls@gmail.com>
2024-08-07 18:02:05 +0900

    24.08.07_Timeline_01

--- ROOT/main/js/index.js
+++ ROOT/main/js/index.js
@@ -77,3 +77,27 @@ function clickLightMode() {
     }
   });
 
+
+  // 내활동
+  const btns = document.querySelectorAll(".faq__btn");
+
+  btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const faqItem = btn.parentNode;
+      const isActive = faqItem.classList.contains("active");
+
+      removeActiveClasses();
+
+      if (!isActive) {
+        faqItem.classList.add("active");
+      }
+    });
+  });
+
+  function removeActiveClasses() {
+    btns.forEach((btn) => {
+      btn.parentNode.classList.remove("active");
+    });
+  }
+
+
